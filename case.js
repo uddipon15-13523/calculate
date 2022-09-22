b96ee847f05e0d6f9149da929699fb2473e4fa2a
uddipon15-13523 <uddipon15-13523@diu.edu.bd>
2022-09-22 10:06:07 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,8 @@
+const lyrics ='tumi bondhu kala pakhi ami jeno ki. bosonto kale tomai bolte pari ni.bodiu l to mi ';
+const parts = lyrics.split(' ');
+const sentences = lyrics.split('.');
+const chars =lyrics.split('');
+
+console.log(parts);
+console.log(sentences);
+console.log(chars);
---
+++ findodd.js
@@ -0,0 +1,10 @@
+function getSumOfAnArray(numbers){
+    for(let i =0; i< numbers.lenght; i++){
+        const index =i;
+        const element = numbers[index]
+        console.log(index,element);
+    }
+    
+}
+const myNumbers = [12,65,45,78,32,45,91];
+getSumOfAnArray(myNumbers);
