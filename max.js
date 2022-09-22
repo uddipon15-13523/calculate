d3471a4c221002d81ef3300590f39b5eefbc022c
uddipon15-13523 <uddipon15-13523@diu.edu.bd>
2022-09-22 11:41:49 +0600

    new add

---
+++ max.js
@@ -0,0 +1,12 @@
+const jim = 88;
+const dela = 99;
+const chinku = 77;
+if(jim > dela && jim > chinku){
+    console.log('jim will get the cak');
+}
+else if(dela >jim && dela > chinku){
+    console.log('dela will get the cak');
+}
+else{
+    console.log('chinju will get the cak');
+}
