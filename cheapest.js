b7e9dcf1c6b3ec61499252fe90e88962f074bfcd
uddipon15-13523 <uddipon15-13523@diu.edu.bd>
2022-09-23 18:11:35 +0600

    mobile

--- cheapest.js
+++ cheapest.js
@@ -8,10 +8,23 @@ const phones =[
     {name:'HTC', camera:12,storage:'32g',price:36000,color:'sliver'},
     
 ];
+// function cheapestphone(phones){
+//     for(let i =0;i<phones.length;i++){
+//         const phone =phones[i];
+//         console.log(phone);
+//     }
+// }
+
+
 function cheapestphone(phones){
+    let cheapest = phones[0];
     for(let i =0;i<phones.length;i++){
         const phone =phones[i];
-        console.log(phone);
+        if(phone.price < cheapest.price){
+            cheapest = phone;
+        }
     }
+    return cheapest;
 }
 const mySelection = cheapestphone(phones);
+console.log(mySelection);
